7583cb9275c8e78cfd3a2ec7f41e1dd8556dd0b1
LaDiamond Kidd <ladiamondk@yahoo.com>
2023-12-01 18:16:06 -0800

    add script to pull form data

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ladiamondk:@registration.aatrzhn.mongodb.net/?retryWrites=true&w=majority";
-const app = express();
-const port = 3000;
 
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  username:String,
+  email:String,
+  password:String,
+  consent:Boolean
 });
+
+const User = mongoose.model('User', userSchema);
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/registration.aatrzhn.mongodb.net', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.post('register', async ()=>{
+  const{username, email, password, consent} = req.body;
+
+  const user = new User({
+    username,
+    email,
+    password,
+    consent
+  });
+  try {
+    await user.save();
+    res.send('User registered successfully!')
+  } catch(error){
+    console.error(error);
+    res.send('Error: User registration failed.')
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000')
+})
--- resources/functionality/javascript.js
+++ resources/functionality/javascript.js
@@ -2,6 +2,15 @@ const signInBtn = document.querySelector('.signInBtn');
 const signUpBtn = document.querySelector('.signUpBtn');
 const signInForm = document.querySelector('#sign-in');
 const signUpForm = document.querySelector('#sign-up');
+const usernameInput = document.getElementById('username');
+const username =  usernameInput.value;
+const emailInput = document.getElementById('email');
+const email =  emailInput.value;
+const passwordInput = document.getElementById('password');
+const password =  passwordInput.value;
+const consentInput = document.getElementById('consent');
+const consent =  consentInput.value;
+
 signUpBtn.addEventListener('click', () => {
     signUpForm.style.visibility = 'visible';
     signInForm.style.visibility = 'hidden';
